b4eda4958f92814b4f476f5a1d5fb25a0b2a1465
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-08 16:50:27 +0530

    swap number commit

---
+++ src/Qn8/swapNumbers/Main.java
@@ -0,0 +1,11 @@
+package Qn8.swapNumbers;
+
+public class Main {
+    public static void main(String[] args) {                                                 //This main method is only for testing purposes
+        swapNumber swapnumber = new swapNumber(5,8);
+        swapnumber.swapNumberByReference();
+        System.out.println("num1="+swapnumber.getNum1() + "num2="+swapnumber.getNum2());
+        swapnumber.swapNumberByValue();
+        System.out.println("num1="+swapnumber.getNum1() + "num2="+swapnumber.getNum2());
+    }
+}
